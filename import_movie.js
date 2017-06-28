6db40395c6eee926828fc1db99d49981c517234b
Anshaj Goel <anshajgoel.cer@gmail.com>
2017-06-28 15:59:21 +0530

    Modules

---
+++ import_movie.js
@@ -0,0 +1,10 @@
+/**
+ * Created by anshaj on 6/28/17.
+ */
+// Lecture 8 - Modules
+//This is how you import a file in the same directory but don't add the extension
+var movies =require("./movies");
+
+movies.avatar();
+
+// This is similar to having public and private methods
---
+++ movies.js
@@ -0,0 +1,14 @@
+/**
+ * Created by anshaj on 6/28/17.
+ */
+// Lecture 8 - Modules
+function printAvatar(){
+    console.log("Avatar: PG-13")
+}
+
+
+function printInterstellar(){
+    console.log("Insterstellar: R")
+}
+
+module.exports.avatar = printAvatar;
