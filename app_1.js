fb0d3a0f4e020f7e7b21f505ae96fdfff8ec0591
Anshaj Goel <anshajgoel.cer@gmail.com>
2017-06-29 19:29:36 +0530

    core modules

--- app_1.js
+++ app_1.js
@@ -2,7 +2,28 @@
  * Created by anshaj on 6/29/17.
  */
 
+//
+// require('./wendy')
+// require('./anshaj')
+//
 
-require('./wendy')
-require('./anshaj')
+// Lecture 12 - Core modules - Read and write files
 
+// var fs = require('fs');
+//
+// fs.writeFileSync("corn.txt","corn is good, its life");
+// console.log(fs.readFileSync("corn.txt").toString())
+
+
+var path = require('path');
+var websiteHome = "~/WebStormProjects/NodeJS_NewBoston/index.html"
+var websiteAbout = "~/WebStormProjects//NodeJS_NewBoston/about.html"
+
+
+console.log(path.normalize(websiteAbout));
+console.log(path.dirname(websiteHome));
+console.log(path.basename(websiteHome));
+console.log(path.extname(websiteHome));
+
+console.log(__dirname);
+console.log(__filename);
